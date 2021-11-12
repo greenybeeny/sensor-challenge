8f825ad3173f393f9040a27a19add14c83054426
greenybeeny <92817241+greenybeeny@users.noreply.github.com>
2021-11-12 13:58:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ let Motion = 0
 basic.forever(function () {
     Motion = pins.digitalReadPin(DigitalPin.P1)
     if (Motion == 0) {
+        basic.showNumber(Motion)
         basic.showIcon(IconNames.No)
-        basic.pause(200)
     } else {
+        basic.showNumber(Motion)
         basic.showIcon(IconNames.Yes)
-        basic.pause(200)
     }
 })
